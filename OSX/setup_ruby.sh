a615f7c65d3347e77d84b883fe83d467ed07a2f6
Masayuki Ono <ono@ripplex.com>
2014-05-07 14:45:14 +0900

    スクリプト更新

--- OSX/setup_ruby.sh
+++ OSX/setup_ruby.sh
@@ -2,8 +2,9 @@ rbenv install -l
 rbenv install 1.9.3-p194
 rbenv install 2.1.1
 # for octopress
-rbenv local 1.9.3-p194
+rbenv shell 1.9.3-p194
 sudo gem install bundler
+rbenv shell 2.1.1
 rbenv global 2.1.1
 sudo gem install bundler
 sudo gem install rails
