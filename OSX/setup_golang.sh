2d3678d7d7c0c372ecb230533c0cb7890cfa8aa5
Masayuki Ono <mono0926@gmail.com>
2014-05-13 04:59:25 +0900

    golang追加

---
+++ OSX/setup_golang.sh
@@ -0,0 +1,3 @@
+bash < <(curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+gvm install go1
+gvm use go1
--- OSX/setup_python.sh
+++ OSX/setup_python.sh
@@ -3,4 +3,5 @@ pyenv install 2.7.6
 pyenv install 3.4.0
 pyenv install pypy-2.2.1
 pyenv global 3.4.0
+pip install docutils
 which python
